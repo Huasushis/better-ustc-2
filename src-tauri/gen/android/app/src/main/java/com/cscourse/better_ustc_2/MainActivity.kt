eff763ae23edb736ce0ce2d43238b8d10d9c5965
Huasushis <huasushi@outlook.com>
2025-11-18 20:03:48 +0800

    refactor: create the project and add some configurations

---
+++ src-tauri/gen/android/app/src/main/java/com/cscourse/better_ustc_2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cscourse.better_ustc_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
